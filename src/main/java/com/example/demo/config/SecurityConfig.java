3bd92d613968eec3f395709ff87c452386b314b6
Ahmad Raihan <ahmad.reyhan2230@gmail.com>
2025-10-15 16:10:40 +0700

    first

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.example.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig{
+
+    @Autowired
+    private JwtConfig jwtConverter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorize)-> authorize
+                        .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(
+                        (oauth2)-> oauth2.jwt(
+                                jwt-> jwt.jwtAuthenticationConverter(jwtConverter)
+                        ))
+                .sessionManagement(
+                        session-> session.sessionCreationPolicy(
+                                SessionCreationPolicy.STATELESS)
+                );
+
+        return http.build();
+    }
+}
